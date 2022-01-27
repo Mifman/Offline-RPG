d060a682eac241f3e5c2163ff1ce6b25e51ecf34
Mifman <art00danger@mail.ru>
2022-01-27 13:50:58 +0300

    Update dunges.py

--- Game RPG/dunges.py
+++ Game RPG/dunges.py
@@ -12,40 +12,40 @@ class Dunge:
 def forest():
     Dunge.name = "Лес"
     Dunge.level_max = rdm.randint(1,3)
-    Dunge.xp = rdm.randint(5,10) / 100
+    Dunge.xp = rdm.randint(5,10) / 1.5
     Dunge.difficulty = rdm.randint(0,1)
 
 # Подземелье
 def dungeon():
     Dunge.name = "Подземелье"
     Dunge.level_max = rdm.randint(2,4)
-    Dunge.xp = rdm.randint(8,18) / 100
+    Dunge.xp = rdm.randint(8,18) / 1.5
     Dunge.difficulty = rdm.randint(2,3)
 
 # Густой лес
 def dense_forest():
     Dunge.name = "Густой лес"
     Dunge.level_max = rdm.randint(3,5)
-    Dunge.xp = rdm.randint(10, 20) / 100
+    Dunge.xp = rdm.randint(10, 20) / 1.5
     Dunge.difficulty = rdm.randint(4,5)
 
 # Заброшенный замок
 def castle():
     Dunge.name = "Заброшенный замок"
     Dunge.level_max = rdm.randint(4,7)
-    Dunge.xp = rdm.randint(18, 28) / 100
+    Dunge.xp = rdm.randint(18, 28) / 1.5
     Dunge.difficulty = rdm.randint(6,7)
 
 # Супер-Подземелье
 def super_dungeon():
     Dunge.name = "Супер-Подземелье"
     Dunge.level_max = rdm.randint(5,7)
-    Dunge.xp = rdm.randint(25, 31) / 100
+    Dunge.xp = rdm.randint(25, 31) / 1.5
     Dunge.difficulty = rdm.randint(8,9)
 
 # Изумрудный лес
 def emerald_forest():
     Dunge.name = "Изумрудный лес"
     Dunge.level_max = rdm.randint(4,6)
-    Dunge.xp = rdm.randint(30, 37) / 100
+    Dunge.xp = rdm.randint(30, 37) / 1.5
     Dunge.difficulty = 10
